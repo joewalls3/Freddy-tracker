e76abd1ed6858356a53f55fca438dfe16c3c9575
Joseph Walls <78870222+joewalls3@users.noreply.github.com>
2026-07-31 13:34:15 -0400

    Refresh map embed module

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 import { loadTrackerData } from "./services/tracker-service.js";
 import { renderDashboard } from "./views/dashboard-view.js";
-import { renderMap } from "./views/map-view.js?v=20260731-map-detail";
+import { renderMap } from "./views/map-view.js?v=20260731-social-embeds";
 import { renderTimeline } from "./views/timeline-view.js";
 import { select, selectAll } from "./utils/dom.js";
 

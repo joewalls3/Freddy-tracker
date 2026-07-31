e430d886956e759210b44d6c1440cc289e0a4a44
Joseph Walls <78870222+joewalls3@users.noreply.github.com>
2026-07-31 13:37:07 -0400

    Rotate embedded-post module cache

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 import { loadTrackerData } from "./services/tracker-service.js";
 import { renderDashboard } from "./views/dashboard-view.js";
-import { renderMap } from "./views/map-view.js?v=20260731-social-embeds";
+import { renderMap } from "./views/map-view.js?v=20260731-social-embeds-ready";
 import { renderTimeline } from "./views/timeline-view.js";
 import { select, selectAll } from "./utils/dom.js";
 
